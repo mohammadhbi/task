93bc9482b1de6eab915616276fe79f9dc880eb95
mohammadhbi <mohammad19hobbi@gmail.com>
2025-06-28 22:49:15 +0330

    git commit -m "feat(auth): fetch user data from API and log first name"

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+import axios from 'axios'
+import React, { useState } from 'react'
 
 export default function page() {
+ const [username,setUsername]=useState();
+  const handleLogin = async ()=>{
+  try {
+    const user = await axios.get("https://randomuser.me/api/?results=1&nat=us")
+    //console.log(user.data);
+    const username1 = user.data.results[0].name.first;
+    setUsername(username1);
+    localStorage.setItem("username", username1);
+  } catch (error) {
+    console.error("something went wrong", error);
+  }
+ }
   return (
     <div>
-      hello
+      <button onClick={handleLogin}> log In</button>
+      <p>{username}</p>
     </div>
-  )
+  );
 }
